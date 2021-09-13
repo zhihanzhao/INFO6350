38e9e90389a8f6a5b02a095adc1d1758a555feb2
Zhihan <zhao.zhiha@husky.neu.edu>
2021-09-13 04:14:38 -0700

    Initial Commit

---
+++ Roll A Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roll A Dice
+//
+//  Created by 赵芷涵 on 9/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
